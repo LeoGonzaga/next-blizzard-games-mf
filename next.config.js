00c4a3578c2d7a7f2cfa98566487ee9fd383279a
LeoGonzaga <sacredlotus05@gmail.com>
2023-01-10 18:16:11 -0300

    feat: adding module federation configuration and expose sidebar to blizzard-app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'components',
+        remotes: {
+          main: `main@http://localhost:3000/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+        },
+        filename: 'static/chunks/remoteEntry.js',
+        exposes:{
+          './sidebar':'./src/components/Sidebar'
+        }
+      })
+    );
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from "./sidebar.module.css"
+
+export const Sidebar: React.FC = () => {
+  return <div className={styles.container} >sidebar</div>;
+}
